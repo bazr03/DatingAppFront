0561bd59ea022404d72f2692482d9dc7ad04540f
Ivan Bazaldua <icesarbr@gmail.com>
2020-08-18 16:08:39 -0500

    third party components added: Angular JWT, ngx-bootstrap and bootswatch

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,28 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
-constructor(private http: HttpClient) { }
+  jwtHelper = new JwtHelperService();
+  constructor(private http: HttpClient) {}
+  decodedToken: any;
 
-  login(model: any){
-    return this.http.post(this.baseUrl + 'login', model)
-      .pipe(
-        map( (res: any) => {
-          const user = res;
-          if (user){
-            localStorage.setItem('token', user.token);
-          }
-        } )
-      );
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((res: any) => {
+        const user = res;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        }
+      })
+    );
   }
 
-  register(model: any){
+  register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
   }
 
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        this.alertify.success('registration ok');
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
   }
 
-  register(){
-    this.authService.register(this.model).subscribe( () => {
-      console.log('registration ok');
-    },
-    error => {
-      console.log(error);
-    } );
-  }
-
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
+    this.alertify.message('cancelled');
   }
 }
