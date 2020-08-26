6da57a65404187132114513a304fcf27f597e4bf
Iván Bazaldúa <icesarbr@gmail.com>
2020-08-26 13:43:25 -0500

    BaheviourSubject added to sync main Photo in all components that need it, next task is add delete photo functionality

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +12,16 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
-  constructor(private http: HttpClient) {}
   decodedToken: any;
+  currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  changeMemberPhoto(photoUrl: string): void {
+    this.photoUrl.next(photoUrl);
+  }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -19,7 +29,10 @@ export class AuthService {
         const user = res;
         if (user) {
           localStorage.setItem('token', user.token);
+          localStorage.setItem('user', JSON.stringify(user.user));
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          this.currentUser = user.user;
+          this.changeMemberPhoto(this.currentUser.photoUrl);
         }
       })
     );
@@ -29,7 +42,7 @@ export class AuthService {
     return this.http.post(this.baseUrl + 'register', model);
   }
 
-  loggedIn() {
+  loggedIn(): boolean {
     const token = localStorage.getItem('token');
     return !this.jwtHelper.isTokenExpired(token);
   }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
   model: any = {};
+  photoUrl: string;
 
   constructor(
     public authService: AuthService,
@@ -17,9 +18,11 @@ export class NavComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.authService.currentPhotoUrl.subscribe( photoUrl => this.photoUrl = photoUrl );
+  }
 
-  login() {
+  login(): void {
     this.authService.login(this.model).subscribe(
       (next) => {
         this.alertify.success('Loggend in successfully');
@@ -33,12 +36,15 @@ export class NavComponent implements OnInit {
     );
   }
 
-  loggedIn() {
+  loggedIn(): boolean {
     return this.authService.loggedIn();
   }
 
-  logout() {
+  logout(): void {
     localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.authService.decodedToken = null;
+    this.authService.currentUser = null;
     this.alertify.message('logged out');
     this.router.navigate(['/home']);
   }
