d31f9db5940af5929d4dd8009c5638de1bd9b4f9
Ivan Bazaldua <icesarbr@gmail.com>
2020-09-16 19:36:17 -0500

    messages-component and member-messages-component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {TimeagoModule} from 'ngx-timeago';
+import { TimeagoModule } from 'ngx-timeago';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
@@ -21,19 +21,20 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { MemberListComponent } from './members/member-list/member-list.component';
-import {MemberCardComponent} from './members/member-card/member-card.component';
-import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
-import {MemberDetailResolver} from './_resolvers/member-detail.resolver';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import {MemberEditResolver} from './_resolvers/member-edit.resolver';
-import {PreventUnsavedChanges} from './_guards/prevent-unsave-changes.guard';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsave-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
-
+import { MessagesResolver } from './_resolvers/messages.resolver';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -52,7 +53,7 @@ export function tokenGetter() {
     MessagesComponent,
     MemberEditComponent,
     PhotoEditorComponent,
-
+    MemberMessagesComponent,
   ],
   imports: [
     BrowserModule,
@@ -73,11 +74,20 @@ export function tokenGetter() {
       config: {
         tokenGetter: tokenGetter,
         allowedDomains: ['localhost:5000'],
-        disallowedRoutes: ['localhost:500/api/auth']
-      }
-    })
+        disallowedRoutes: ['localhost:500/api/auth'],
+      },
+    }),
   ],
-  providers: [AuthService, ErrorInterceptorProvider, MemberDetailResolver, MemberListResolver, MemberEditResolver, PreventUnsavedChanges, ListsResolver],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+    MemberDetailResolver,
+    MemberListResolver,
+    MemberEditResolver,
+    PreventUnsavedChanges,
+    ListsResolver,
+    MessagesResolver
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
